88677610e3f61697d7bb9ce71bc396f0049e1d2c
Dmitri Park <cellofortist@gmail.com>
2013-05-02 22:32:06 -0400

    ok.

---
+++ lib/DDG/Goodie/Didi.pm
@@ -0,0 +1,19 @@
+package DDG::Goodie::Didi;
+use DDG::Goodie;
+
+triggers any => 'didi', 'Didi', 'didigodot';
+
+primary_example_queries 'who is Didi?';
+secondary_example_queries 'Didi Park';
+description 'print my name because I'm awesome';
+name 'Didi';
+code_url 'https://github.com/didigodot/zeroclickinfo-goodies/lib/DDG/Goodie/Didi.pm';
+category 'special';
+topics 'random';
+attribution github => [ 'http://github.com/didigodot', 'didigodot' ];
+
+handle query_lc => sub {
+	return "Didi is awesome";
+	return;
+};
+1;
